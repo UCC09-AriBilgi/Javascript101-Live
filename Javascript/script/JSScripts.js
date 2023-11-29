b1ce998f0d245b81991ab8202e26aa63bcd2ec5d
ukaracivio <umit.karacivi@outlook.com>
2023-11-29 13:55:05 +0300

    Some changes

--- Javascript/script/JSScripts.js
+++ Javascript/script/JSScripts.js
@@ -63,3 +63,15 @@ function pencerekapat() {
         window.close();
     }
 }
+
+function daralt(){
+    document.getElementById("kutu").className = "dar";
+}
+
+function normal(){
+    document.getElementById("kutu").className = "normal";
+}
+
+function genislet(){
+    document.getElementById("kutu").className = "genis";
+}
