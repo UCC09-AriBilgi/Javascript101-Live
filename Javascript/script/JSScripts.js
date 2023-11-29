f0f23e669f615d4d7bde48c3a55c7c0deb91fef3
ukaracivio <umit.karacivi@outlook.com>
2023-11-29 11:32:37 +0300

    Some changes

--- Javascript/script/JSScripts.js
+++ Javascript/script/JSScripts.js
@@ -4,5 +4,28 @@
 
 function degistir() {
     // ekrandaki varolan bir element(tag) içeriği ile oynamak
-    document.getElementById("demo").innerHTML = "Parafraf şimdi değişti";
+    document.getElementById("demo").innerHTML = "Paragraf şimdi değişti";
+}
+
+function tarihgoster() {
+    // dd.mm.yyyy ??
+    document.write("Bügünün tarihi : " + Date())
+}
+
+function sayigoster() {
+    var sayi1 = document.getElementById("sayi1").value;
+    var sayi2 = document.getElementById("sayi2").value;
+
+    var gosterilecek = "Girmiş olduğunuz sayilar : " + sayi1 + " ve " + sayi2 + " dir..";
+    document.getElementById("Mesaj").innerHTML = gosterilecek;
+
+}
+
+function topla() {
+    var sayi1 = parseInt(document.getElementById("sayi1").value);
+    var sayi2 = parseInt(document.getElementById("sayi2").value);
+
+    var gosterilecek = sayi1 + sayi2;
+
+    document.getElementById("Mesaj").innerHTML = "Toplam = " + gosterilecek;
 }
