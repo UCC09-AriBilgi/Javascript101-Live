957eb0a3277ba4c928fff5ff87301708d92f1461
ukaracivio <umit.karacivi@outlook.com>
2023-11-29 10:35:09 +0300

    Add project files.

---
+++ Javascript/script/JSScripts.js
@@ -0,0 +1,8 @@
+﻿function hw() {
+    alert("Javascript dünyasına hoş geldiniz...");
+}
+
+function degistir() {
+    // ekrandaki varolan bir element(tag) içeriği ile oynamak
+    document.getElementById("demo").innerHTML = "Parafraf şimdi değişti";
+}
