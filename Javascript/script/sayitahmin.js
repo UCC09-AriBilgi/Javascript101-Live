0de50d902a31c63ca08d41d5167f3b6d6fabc04e
ukaracivio <umit.karacivi@outlook.com>
2023-11-30 15:03:46 +0300

    Some changes

---
+++ Javascript/script/sayitahmin.js
@@ -0,0 +1,26 @@
+﻿function yaz() {
+    var tutulansayi = Math.floor(Math.random() * 10);
+
+    var tahmin;
+
+    var sayac;
+
+    while (tahmin != tutulansayi) {
+        tahmin = eval(prompt("Tahminini yap", ""));
+        sayac++;
+
+        document.getElementById("tahminler").innerHTML += tahmin + " ";
+
+        if (tahmin < tutulansayi) {
+            document.getElementById("ipucu").innerHTML = sayac + "- YUKARI";
+        }
+
+        if (tahmin > tutulansayi) {
+            document.getElementById("ipucu").innerHTML = sayac + "- AŞAĞI";
+        }
+
+    }
+
+    document.getElementById("snc").innerHTML = "TEBRİKLEEERRR " + sayac + ".denemede buldunuz...";
+    document.getElementById("ipucu").innerHTML = "";
+}
